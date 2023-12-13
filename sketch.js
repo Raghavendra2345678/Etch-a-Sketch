9a0951e5d2394980db79bd5679849cf0df37ffd1
Raghavendra2345678 <raghavendraamberkar07@gmail.com>
2023-12-13 11:33:07 +0530

    grid is now displayed correctly

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function addBoxes() {
+    const n = document.getElementById("in").value;
+    console.log(n);
+    squares(n);
+}
+
+function squares(n) {
+    
+    const c = document.getElementById("container");
+    console.log(c.style.width);
+    for(let i = 0; i < n; i++){
+        const row = document.createElement("div");
+        row.style.width= "410px";
+        row.style.height= 410/n+"px";
+        console.log("hello");
+        for (let j = 0; j < n; j++) {
+            const sq = document.createElement('div');
+            sq.style.backgroundColor = "red";
+            sq.style.width = 410/n+"px";
+            sq.style.height = 410/n+"px";
+            sq.style.flex=1;
+            row.appendChild(sq);
+        }
+        console.log('hi');
+        c.appendChild(row);
+        
+    }
+}
+
