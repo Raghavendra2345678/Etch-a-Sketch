c976e90cfc37a65a362e33c6209938a3da0f2d3e
Raghavendra2345678 <raghavendraamberkar07@gmail.com>
2023-12-15 22:53:55 +0530

    Final edit-1

--- sketch.js
+++ sketch.js
@@ -1,25 +1,26 @@
 function addBoxes() {
     const n = document.getElementById("in").value;
-    console.log(n);
+    // console.log(n);
     squares(n);
 }
 
+//function to generate matrix of small divs(squares)
 function squares(n) {
     
     const c = document.getElementById("container");
     console.log(c.style.width);
-    
+    c.innerHTML="";
     for(let i = 0; i < n; i++){
         const row = document.createElement("div");
         row.style.display="flex";
-        row.style.width= "410px";
-        row.style.height= 410/n+"px";
+        row.style.width= "460px";
+        row.style.height= 460/n+"px";
         //console.log("hello");
         for (let j = 0; j < n; j++) {
             const sq = document.createElement('div');
             sq.id = "sq";
-            sq.style.width = 410/n+"px";
-            sq.style.height = 410/n+"px";
+            sq.style.width = 460/n+"px";
+            sq.style.height = 460/n+"px";
             sq.style.flex = 1;
             sq.style.flexShrink= 0;
             sq.onclick="sq.style.background-color: grey;";
@@ -32,8 +33,9 @@ function squares(n) {
 }
 
 function reset_grid(){
-    var container = document.getElementById("container");
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
+
+    const c = document.getElementById("container");
+    const n = document.getElementById("in").value;
+    c.innerHTML = "";
+    squares(n);
 }
